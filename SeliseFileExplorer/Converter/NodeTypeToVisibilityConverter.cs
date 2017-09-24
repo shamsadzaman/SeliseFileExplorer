c4817b865dcc65e4e4f6974728a4f3b514f83ea7
szaman <shamsadzaman@yahoo.com>
2017-09-25 00:45:29 +0600

    show folder icon in folder view

---
+++ SeliseFileExplorer/Converter/NodeTypeToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using SeliseFileExplorer.Constants;
+
+namespace SeliseFileExplorer.Converter
+{
+    [MarkupExtensionReturnType(typeof(NodeTypeToVisibilityConverter))]
+    public class NodeTypeToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        private static readonly NodeTypeToVisibilityConverter Instance = new NodeTypeToVisibilityConverter();
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Instance;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is NodeType) || !(parameter is NodeType))
+            {
+                return Visibility.Collapsed;
+            }
+
+            var nodeType = (NodeType) value;
+            var targetNodeType = (NodeType) parameter;
+
+            if (nodeType == targetNodeType)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
