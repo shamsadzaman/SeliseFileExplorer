e42224d72015dcd202e8c54c1a2ea055e0e6996f
szaman <shamsadzaman@yahoo.com>
2017-09-30 14:25:53 +0600

    round/square image on theme chagne

---
+++ SeliseFileExplorer/Converter/ThemeTypeToCollapsedConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using SeliseFileExplorer.Model;
+
+namespace SeliseFileExplorer.Converter
+{
+    public class ThemeTypeToCollapsedConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ThemeId && parameter is ThemeId)
+            {
+                var themeId = (ThemeId) value;
+                var targetThemeId = (ThemeId) parameter;
+                if (themeId == targetThemeId)
+                {
+                    return Visibility.Visible;
+                }
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
